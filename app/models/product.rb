37bd9612b400dbe93397cc4d21a23c47771e2b16
LED <fullchaoz32@yahoo.com>
2012-02-17 16:26:11 +0800

    Unit testing added

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,18 @@
 class Product < ActiveRecord::Base
+  validates :title , :description ,:image_url, :presence =>true
+  validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+  validates :title, :uniqueness => true
+=begin
+ 
+  
+    validates :image_url, :format => {
+  :with => %r{\.(gif|jpg|png)$}i,
+  :message => 'must be a URL for GIF, JPG or PNG image.'
+  }
+=end
+  
+  validates :image_url, :format => {
+    :with => %r{\.(gif|jpg|png)$}i,
+    :message => 'must be a Gif,Jpg,PNG file format pictures.'
+  }
 end
