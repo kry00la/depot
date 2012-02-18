84a6c6329fae1d2993f99ebe04508282deb74039
LED <fullchaoz32@yahoo.com>
2012-02-19 01:21:08 +0800

    added cart and cart test

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,7 @@
 class StoreController < ApplicationController
   def index
     @product = Product.all
+    @count = session_count
   end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,16 +1,22 @@
 class Product < ActiveRecord::Base
   default_scope :order => 'title'
+  has_many :line_items
+  
+  before_destroy :ensure_not_referenced_by_any_line_item
+  
+  def ensure_not_referenced_by_any_line_item
+    if line_items.count.zero?
+      return true
+    else
+      errors[:base] << "Line Items present"
+      return false
+    end
+  end
+  
+  default_scope :order => 'title'
   validates :title , :description ,:image_url, :presence =>true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
   validates :title, :uniqueness => true
-=begin
- 
-  
-    validates :image_url, :format => {
-  :with => %r{\.(gif|jpg|png)$}i,
-  :message => 'must be a URL for GIF, JPG or PNG image.'
-  }
-=end
   
   validates :image_url, :format => {
     :with => %r{\.(gif|jpg|png)$}i,
